b0390339dfe3d5330d0a141425d5a071865530a4
dpvpd <61593643+dpvpd@users.noreply.github.com>
2024-05-28 00:43:28 +0900

    first commit

---
+++ app/src/main/java/com/epinephrine00/buddyjet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.epinephrine00.buddyjet
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
